46a2074884275b76f5d41726aa305398851f5a0e
Chiller Cat <81083840+Eternal587@users.noreply.github.com>
2022-09-18 19:30:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-var usersguess = readLine("what the password");
-var password = ("thisisntapassword")
-if (password = usersguess) {
-window.location.replace("eternal587.github.io/TheBedroom/main.html");
-}
-else {
-readline("dumbass")
-}
+let Usersguess = window.prompt("whats the password")
+console.log(usersguess)
