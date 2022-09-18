df29588b0202e718128ced763ba92910b0197465
Chiller Cat <81083840+Eternal587@users.noreply.github.com>
2022-09-18 19:05:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-var x = document.createElement("INPUT");
-x.setAttribute("type", "text");
+var usersguess = readLine("what the password");
+var password = ("thisisntapassword")
+if (password = usersguess) {
+window.location.replace("eternal587.github.io/TheBedroom/main.html");
+}
+else {
+readline("dumbass")
+}
