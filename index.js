9cb539c34b49dfa522faac78f312f719ada576b5
Chiller Cat <81083840+Eternal587@users.noreply.github.com>
2022-09-24 09:54:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let password = "this is a password";
 if (password === usersguess) {
    window.location.replace("https://eternal587.github.io/TheBedroom/main.html");
    document.cookie = "passright";
+   localStorage.setItem("visit", ".");
 }
 else {
 let Usersguess = window.prompt("last try before i smack you");
